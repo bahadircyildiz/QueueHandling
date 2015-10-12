2c7dd30d90ccb4886415b4071422623f1da30c3d
Bahadir Can Yildiz <bahadircyildiz@yandex.com>
2015-10-12 22:43:45 +0300

    Stable Version

--- scripts/controller.js
+++ scripts/controller.js
@@ -28,7 +28,7 @@ function Controller($scope){
 	
 	$scope.start = function(){
 		var processes = angular.copy($scope.processes);
-		var systemClock = 0, arrivalTime = processes[0].interArrivalTime , departTime1 = Infinity, departTime2 = Infinity;
+		var systemClock = 0, arrivalTime = processes[0].interArrivalTime , departTime1 = Infinity, departTime2 = Infinity, d1iIsSet = false, d2IsSet = false;
 		var queue1 = [], queue2 = []; 
 		var logs = [], log = {};
 		log = {systemClock: systemClock, arrivalTime: arrivalTime, departTime1: departTime1, departTime2: departTime2, queue1: queue1.length, queue2: queue2.length};
@@ -38,7 +38,10 @@ function Controller($scope){
 			arrivalTime = systemClock + processes[0].interArrivalTime;
 			queue1.push(processes[0]);
 			processes.splice(0,1);
-			departTime1 = systemClock + queue1[0].serverTime1;
+			if(!d1iIsSet) {
+				departTime1 = systemClock + queue1[0].serverTime1;
+				d1iIsSet = true;
+			}
 			if(processes.length == 0) arrivalTime = Infinity; else arrivalTime = systemClock + processes[0].interArrivalTime;
 			log = {systemClock: systemClock, arrivalTime: arrivalTime, departTime1: departTime1, departTime2: departTime2, queue1: queue1.length, queue2: queue2.length};
 			logs.push(log);
@@ -48,12 +51,17 @@ function Controller($scope){
 			queue2.push(queue1[0]);
 			queue1.splice(0,1);
 			queue1[0] ? departTime1 = systemClock + queue1[0].serverTime1 : departTime1 = Infinity;
-			departTime2 = systemClock + queue2[0].serverTime2;
+			d1iIsSet = false;
+			if(!d2IsSet){
+				departTime2 = systemClock + queue2[0].serverTime2;
+				d2IsSet = true;
+			}
 			log = {systemClock: systemClock, arrivalTime: arrivalTime, departTime1: departTime1, departTime2: departTime2, queue1: queue1.length, queue2: queue2.length};
 			logs.push(log);
 		}
 		var depart2 = function(){
 			queue2.splice(0,1);
+			d2IsSet = false;
 			queue2[0] ? departTime2 = systemClock + queue2[0].serverTime2 : departTime2 = Infinity;
 			log = {systemClock: systemClock, arrivalTime: arrivalTime, departTime1: departTime1, departTime2: departTime2, queue1: queue1.length, queue2: queue2.length};
 			logs.push(log);
