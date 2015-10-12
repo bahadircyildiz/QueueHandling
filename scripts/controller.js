fb9d54d09fe18576f1a5d8f5476ce5e10ff0ed08
Bahadir Can Yildiz <bahadircyildiz@yandex.com>
2015-10-12 21:58:07 +0300

    Queue Bugs Fixed

--- scripts/controller.js
+++ scripts/controller.js
@@ -27,11 +27,11 @@ function Controller($scope){
 	
 	
 	$scope.start = function(){
-		var processes = $scope.processes;
+		var processes = angular.copy($scope.processes);
 		var systemClock = 0, arrivalTime = processes[0].interArrivalTime , departTime1 = Infinity, departTime2 = Infinity;
 		var queue1 = [], queue2 = []; 
 		var logs = [], log = {};
-		log = {systemClock: systemClock, arrivalTime: arrivalTime, depart1: departTime1, depart2: departTime2, queue1: queue1.length, queue2: queue2.length};
+		log = {systemClock: systemClock, arrivalTime: arrivalTime, departTime1: departTime1, departTime2: departTime2, queue1: queue1.length, queue2: queue2.length};
 		logs.push(log);
 		systemClock = processes[0].interArrivalTime;
 		var arrive = function(){
@@ -40,33 +40,36 @@ function Controller($scope){
 			processes.splice(0,1);
 			departTime1 = systemClock + queue1[0].serverTime1;
 			if(processes.length == 0) arrivalTime = Infinity; else arrivalTime = systemClock + processes[0].interArrivalTime;
-			log = {systemClock: systemClock, arrivalTime: arrivalTime, depart1: departTime1, depart2: departTime2, queue1: queue1.length, queue2: queue2.length};
+			log = {systemClock: systemClock, arrivalTime: arrivalTime, departTime1: departTime1, departTime2: departTime2, queue1: queue1.length, queue2: queue2.length};
 			logs.push(log);
 		}
 		
 		var depart1 = function(){
 			queue2.push(queue1[0]);
 			queue1.splice(0,1);
-			queue1[0] ? departTime1 = serverTime1 + queue1[0].serverTime1 : departTime1 = Infinity;
+			queue1[0] ? departTime1 = systemClock + queue1[0].serverTime1 : departTime1 = Infinity;
 			departTime2 = systemClock + queue2[0].serverTime2;
-			log = {systemClock: systemClock, arrivalTime: arrivalTime, depart1: departTime1, depart2: departTime2, queue1: queue1.length, queue2: queue2.length};
+			log = {systemClock: systemClock, arrivalTime: arrivalTime, departTime1: departTime1, departTime2: departTime2, queue1: queue1.length, queue2: queue2.length};
 			logs.push(log);
 		}
 		var depart2 = function(){
 			queue2.splice(0,1);
 			queue2[0] ? departTime2 = systemClock + queue2[0].serverTime2 : departTime2 = Infinity;
-			log = {systemClock: systemClock, arrivalTime: arrivalTime, depart1: departTime1, depart2: departTime2, queue1: queue1.length, queue2: queue2.length};
+			log = {systemClock: systemClock, arrivalTime: arrivalTime, departTime1: departTime1, departTime2: departTime2, queue1: queue1.length, queue2: queue2.length};
 			logs.push(log);
 		}
 		
 		while (processes.length != 0 || queue1.length != 0 || queue2.length != 0){
 			systemClock = Math.min(arrivalTime, departTime1, departTime2);
+			if(systemClock == Infinity) break;
 			if(systemClock == arrivalTime) arrive();
 			systemClock = Math.min(arrivalTime, departTime1, departTime2);
+			if(systemClock == Infinity) break;
 			if(systemClock == departTime1) depart1();
 			systemClock = Math.min(arrivalTime, departTime1, departTime2);
+			if(systemClock == Infinity) break;
 			if(systemClock == departTime2) depart2();
 		}
-				
+		$scope.logs = logs;		
 	}		
 }
